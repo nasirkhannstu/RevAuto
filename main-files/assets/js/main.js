87cf877b496c45bd1b9f99a13ca07ccb8d8b84d9
Hozaifa Bin Masud <hozaifabinmasud@gmail.com>
2024-12-08 15:35:24 +0600

    Add swiper without pagination

--- main-files/assets/js/main.js
+++ main-files/assets/js/main.js
@@ -170,6 +170,17 @@ CSS TABLE OF CONTENTS
       });
     }
 
+    // hero 4 slider
+    const swiper = new Swiper(".hero-4-swiper", {
+      loop: true, // Enable infinite loop
+      spaceBetween: 10, // Space between slides
+      slidesPerView: 1, // Show 1 slide at a time
+      pagination: {
+        el: ".hero-4-pagination", // Enable pagination
+        clickable: true, // Make it clickable
+      },
+    });
+
     //>> Testimonial-slider Slider Start <<//
     if ($(".testimonial-slider").length > 0) {
       const testimonialSlider = new Swiper(".testimonial-slider", {
